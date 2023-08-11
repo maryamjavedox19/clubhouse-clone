45a238869018d7ba47598b74bbf4c60260efd434
Maryam <maryamjaved.xo@gmail.com>
2023-08-11 09:22:46 +0500

    welcome page heading and paragraph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import './App.css';
 import Welcome from './pages/Welcome';
+import PlanLayout from './pages/Layouts/PlanLayout';
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 
 function App() {
   return (
+    <BrowserRouter>
+      <PlanLayout>
+        <Welcome />
+      </PlanLayout></BrowserRouter>
 
-    <Welcome></Welcome>
+
   );
 
 }
