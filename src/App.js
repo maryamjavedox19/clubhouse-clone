384da68bfc09332198de919f4be47f3000d635df
Maryam <maryamjaved.xo@gmail.com>
2023-08-20 14:49:59 +0500

    code confirmation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 import Welcome from "./pages/Welcome";
 import PlanLayout from "./pages/Layouts/PlanLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PhoneConfirmation from './pages/PhoneConfirmation';
-import { Component } from "react";
+import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirm from "./pages/CodeConfirm";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Welcome} />
           <Route exact path="/invite" Component={PhoneConfirmation} />
+          <Route exact path="/code-confirm" Component={CodeConfirm}></Route>
         </Routes>
       </PlanLayout>
     </BrowserRouter>
