d89056d9d379ed27825618837f1db6b5bb24a95d
Maryam <maryamjaved.xo@gmail.com>
2023-08-21 12:39:37 +0500

    home button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
-import Header from "../components/Header"
+import Header from "../components/Header";
 import DailyInfoCard from "../components/DailyInfoCard";
 import RoomInfoCard from "../components/RoomInfoCard";
-
-
-
+import style from "../style/home.module.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsGrid3X3Gap } from "react-icons/bs";
 
 export default function Home() {
     return (
         <>
             <Header />
-            <DailyInfoCard />
-            <RoomInfoCard />
-
+            <div className={style.home_container}>
+                <DailyInfoCard />
+                <RoomInfoCard />
+            </div>
+            <div className={style.action_btn}>
+                <button>
+                    <AiOutlinePlus className="mr-2" />
+                    Start a room
+                </button>
+                <button>
+                    <BsGrid3X3Gap />
+                </button>
+            </div>
         </>
-
-
-
-    )
+    );
 }
